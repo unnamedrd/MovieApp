9f55f4267d86fc8f62e9729bfd6170a8772e5d10
najwanan <theunnamedrd@gmail.com>
2022-08-06 23:39:18 -0700

    debugging server.js file

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //jquery
 //debug template literal check formatting 
 
-$(document).ready(function () {
+/*$(document).ready(function () {
     $('#title').autocomplete({
         source: async function (request, response) {
-            let data = await fetch(`http://localhost:8000/search?query=${request.term}`, {mode: 'cors'})
+            let data = await fetch(`http://localhost:8700/search?query=${request.term}`, {mode: 'cors'})
                 //fetch something, get results back and turn into JSON
                 .then(results => results.json())
                 .then(results => results.map(result => {
@@ -19,7 +19,7 @@ $(document).ready(function () {
         minLength: 2, 
         select: function (event, ui) {
             console.log(ui.item.id)
-            fetch(`http://localhost:8000/get/${ui.item.id}`)
+            fetch(`http://localhost:8700/get/${ui.item.id}`)
                 .then(result => result.json())
                 .then(result => {
                     $('#cast').empty()
@@ -32,4 +32,40 @@ $(document).ready(function () {
         }
     })
        
-})
+})*/
+
+$(document).ready(function () {
+  $("#title").autocomplete({
+    source: async function (request, response) {
+      let data = await fetch(
+        `http://localhost:3000/search?query=${request.term}`
+      )
+        .then((results) => results.json())
+        .then((results) =>
+          results.map((result) => {
+            return {
+              label: result.title,
+              value: result.title,
+              id: result._id,
+            };
+          })
+        );
+      response(data);
+      //console.log(response)
+    },
+    minLength: 2,
+    select: function (event, ui) {
+      console.log(ui.item.id);
+      fetch(`http://localhost:3000/get/${ui.item.id}`)
+        .then((result) => result.json())
+        .then((result) => {
+          $("#cast").empty();
+          result.cast.forEach((cast) => {
+            $("#cast").append(`<li>${cast}</li>`);
+          });
+          $("img").attr("src", result.poster);
+        });
+    },
+  });
+});
+
